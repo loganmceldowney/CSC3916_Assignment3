c58569113e1efcbfeef421c29eee42af84074963
loganmceldowney <79785851+loganmceldowney@users.noreply.github.com>
2023-03-06 18:32:45 -0700

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -5,10 +5,11 @@ mongoose.connect(process.env.DB);
 
 // Movie schema
 var MovieSchema = new Schema({
-    title: { type: String, required: true },
-    release_date: { type: Date, required: true },
-    genre: { type: String },
-    actors: [{ type: String }],
+    title: { type: String, required: true, index: { unique: true }},
+    releaseYear: { type: String, required: true},
+    genre: { type: String, required: true, enum: ['Horror', 'Action', 'Drama', 'Comedy', 'Romance', 'Sci-fi', 'Documentary', 'Musical', 'Thriller']},
+    actors: { type: [{actorName: String, characterName: String}], required: true },
+    imageURL: String
 });
 
 // return the model
